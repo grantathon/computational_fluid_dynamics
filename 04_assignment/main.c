2cd7aa5156e93650194050c223ef9010e3025782
Grant <grant.bartel@gmail.com>
2014-05-26 15:28:09 +0200

    Copied assignment 1, implemented dt_value for visualization output

--- 04_assignment/main.c
+++ 04_assignment/main.c
@@ -4,7 +4,6 @@
 #include "uvp.h"
 #include "boundary_val.h"
 #include "sor.h"
-#include "string.h"
 #include <stdio.h>
 
 /**
@@ -40,43 +39,37 @@
  *   iteration loop the operation sor() is used.
  * - calculate_uv() Calculate the velocity at the next time step.
  */
-int main(int argc, char *argv[])
+int main(int argn, char** args)
 {
 	/* Input file with user parameters */
-	const char *problem = argv[1];
-	char *problemDataFile = malloc(strlen(problem) + 5);
-	char *problemOutput = malloc(strlen(problem) + 7);
+	const char *szFileName = "cavity100.dat";
+	const char *szProblem = "SciCompCFD_Assignment_01";
 	int readParamError = 0;
 
 	/* Geometry data */
-	double xlength 	= 0;
-	double ylength 	= 0;
-	int imax 		= 0;
-	int jmax 		= 0;
-	double dx 		= 0;
-	double dy 		= 0;
-
-	/* Domain boundary Condition variables */
-	int wl = 0;
-	int wr = 0;
-	int wt = 0;
-	int wb = 0;
+	double xlength = 0;
+	double ylength = 0;
+	int imax = 0;
+	int jmax = 0;
+	double dx = 0;
+	double dy = 0;
 
 	/* Time-stepping data */
-	double t 		= 0;
-	double t_end 	= 0;
-	double dt 		= 0;
-	double tau 		= 0;
+	double t = 0;
+	double t_end = 0;
+	double dt = 0;
+	double tau = 0;
 	double dt_value = 0;
-	int n 			= 0;
+	double visual_n = 1.0;
+	int n = 0;
 
 	/* Pressure iteration data */
-	int itermax 	= 0;
-	int it 			= 0;
-	double res 		= 0;
-	double eps 		= 0;
-	double omg 		= 0;
-	double alpha 	= 0;
+	int itermax = 0;
+	int it = 0;
+	double res = 0;
+	double eps = 0;
+	double omg = 0;
+	double alpha = 0;
 
 	/* Problem dependent quantities */
 	double Re = 0;
@@ -85,36 +78,20 @@ int main(int argc, char *argv[])
 	double UI = 0;
 	double VI = 0;
 	double PI = 0;
-	double delta_p	= 0;	/* for pressure BC */
-	double Pw = 0;			/* pressure value at right boundary */
 
 	/* Resulting system quantities */
-	double **U 	= 0;
-	double **V 	= 0;
-	double **P 	= 0;
+	double **U = 0;
+	double **V = 0;
+	double **P = 0;
 	double **RS = 0;
-	double **F 	= 0;
-	double **G 	= 0;
-	int **Flag 	= 0;
-
-	/* User must enter correct problem strings */
-	if(strcmp(problem, "Karman_vortex") && strcmp(problem, "plane_shear_flow") && strcmp(problem, "flow_over_a_step"))
-	{
-		printf("Run program using command './sim [problem]', "
-				"where problem is 'Karman_vortex', 'plane_shear_flow', or 'flow_over_a_step'\n");
-		return 0;
-	}
-	strcpy(problemDataFile, problem);
-	strcat(problemDataFile, ".dat");
-
-	/* Append name for output vtk file */
-	strcpy(problemOutput, problem);
-	strcat(problemOutput, "_Output");
+	double **F = 0;
+	double **G = 0;
 
 	/* Read parameters from DAT file, store locally, and check for potential error */
-	readParamError = read_parameters(problemDataFile, &Re, &UI, &VI, &PI, &GX, &GY,
+	readParamError = read_parameters(szFileName, &Re, &UI, &VI, &PI, &GX, &GY,
 			&t_end, &xlength, &ylength, &dt, &dx, &dy, &imax, &jmax, &alpha, &omg, &tau,
-			&itermax, &eps, &dt_value, &wl, &wr, &wt, &wb, &delta_p, &Pw);
+			&itermax, &eps, &dt_value);
+
 	if(readParamError != 1)
 	{
 		printf("ERROR: Input parameters potentially corrupt!");
@@ -127,39 +104,36 @@ int main(int argc, char *argv[])
 	F = matrix(0, imax+1, 0, jmax+1);
 	G = matrix(0, imax+1, 0, jmax+1);
 
-	/* Init flags indicating cell type and neighbours cell types */
-	init_flag(problem, imax, jmax, &Flag);
-
 	/* Begin the time iteration process */
-	printf("Begin the main computation...\n");
 	while(t < t_end)
 	{
 		calculate_dt(Re, tau, &dt, dx, dy, imax, jmax, U, V);
-		boundaryvalues(imax, jmax, U, V, wl, wr, wt, wb, Flag);
-
-		spec_boundary_val(problem, imax, jmax, U, V, UI, VI);
-
-		calculate_fg(Re, GX, GY, alpha, dt, dx, dy, imax, jmax, U, V, F, G, Flag);
+		boundaryvalues(imax, jmax, U, V);
+		calculate_fg(Re, GX, GY, alpha, dt, dx, dy, imax, jmax, U, V, F, G);
 		calculate_rs(dt, dx, dy, imax, jmax, F, G, RS);
 
 		it = 0;
 		do
 		{
-			sor(omg, dx, dy, imax, jmax, P, RS, &res, Flag, Pw, delta_p);
+			sor(omg, dx, dy, imax, jmax, P, RS, &res);
 			it++;
 		}
-		while( it < (itermax) && res > eps);
-		printf("n=%u, res=%f, it=%u, ", n, res, it);
+		while( it < itermax && res > eps);
+		printf("res=%f, it=%u ", res, it);
 
-		calculate_uv(dt, dx, dy, imax, jmax, U, V, F, G, P, Flag);
+		calculate_uv(dt, dx, dy, imax, jmax, U, V, F, G, P);
+
+		/* Visualize U, V, and P depending on dt_value */
+		if(((t / dt_value) >= visual_n) || (t == dt))
+		{
+			write_vtkFile(szProblem, visual_n, xlength, ylength, imax, jmax, dx, dy, U, V, P);
+			visual_n++;
+		}
 
 		n++;
 		t += dt;
 		printf("t=%f, dt=%f\n", t, dt);
 	}
-
-	/* Visualize U, V, and P */
-	write_vtkFile(problemOutput, n, xlength, ylength, imax, jmax, dx, dy, U, V, P);
 
 	/* Deallocate heap memory */
 	free_matrix(U, 0, imax+1, 0, jmax+1);
@@ -168,7 +142,6 @@ int main(int argc, char *argv[])
 	free_matrix(RS, 0, imax+1, 0, jmax+1);
 	free_matrix(F, 0, imax+1, 0, jmax+1);
 	free_matrix(G, 0, imax+1, 0, jmax+1);
-	free_imatrix(Flag, 0, imax+1, 0, jmax+1);
 
 	return -1;
 }
---
+++ 04_assignment/uvp.c
@@ -0,0 +1,210 @@
+#include "math.h"
+#include "uvp.h"
+
+double matrix_abs_max(double **A, int imax, int jmax)
+{
+	int i, j;
+	double max_val = 0;
+
+	/* Iterate only over the inner cells */
+	for(i = 1; i < imax+1; i++)
+	{
+		for(j = 1; j < jmax+1; j++)
+		{
+			if(fabs(A[i][j]) > max_val)
+			{
+				/* Only grab the maximum absolute value */
+				max_val = fabs(A[i][j]);
+			}
+		}
+	}
+
+	return max_val;
+}
+
+void calculate_fg(
+  double Re,
+  double GX,
+  double GY,
+  double alpha,
+  double dt,
+  double dx,
+  double dy,
+  int imax,
+  int jmax,
+  double **U,
+  double **V,
+  double **F,
+  double **G
+)
+{
+	int i, j;
+	double du_x_2, du_y_2, dv_x_2, dv_y_2, duv_x, duv_y, du2_x, dv2_y;
+
+	/* Iterate only over the inner cells of F */
+	for(i = 1; i <= imax-1; i++)
+	{
+		for(j = 1; j <= jmax; j++)
+		{
+			/* Setup 1st and 2nd order derivatives for U and V */
+			du_x_2 = (U[i-1][j] - 2*U[i][j] + U[i+1][j])/pow(dx, 2);
+			du_y_2 = (U[i][j-1] - 2*U[i][j] + U[i][j+1])/pow(dy, 2);
+
+			duv_y = (1/dy)*((((V[i][j] + V[i+1][j])*(U[i][j] + U[i][j+1]))/4)
+						- (((V[i][j-1] + V[i+1][j-1])*(U[i][j-1] + U[i][j]))/4))
+					+ (alpha/dy)*(((fabs(V[i][j] + V[i+1][j])*(U[i][j] - U[i][j+1]))/4)
+							- ((fabs(V[i][j-1] + V[i+1][j-1])*(U[i][j-1] - U[i][j]))/4));
+
+			du2_x = (1/dx)*(pow((U[i][j] + U[i+1][j])/2, 2) - pow((U[i-1][j] + U[i][j])/2, 2))
+					+ (alpha/dx)*(((fabs(U[i][j] + U[i+1][j])*(U[i][j] - U[i+1][j]))/4)
+							- ((fabs(U[i-1][j] + U[i][j])*(U[i-1][j] - U[i][j]))/4));
+
+			F[i][j] = U[i][j] + dt*(((du_x_2 + du_y_2)/Re) - du2_x - duv_y + GX);
+		}
+	}
+
+	/* Iterate only over the inner cells of G */
+	for(i = 1; i <= imax; i++)
+	{
+		for(j = 1; j <= jmax-1; j++)
+		{
+			/* Setup 1st and 2nd order derivatives for U and V */
+			dv_x_2 = (V[i-1][j] - 2*V[i][j] + V[i+1][j])/pow(dx, 2);
+			dv_y_2 = (V[i][j-1] - 2*V[i][j] + V[i][j+1])/pow(dy, 2);
+
+			duv_x = (1/dx)*((((U[i][j] + U[i][j+1])*(V[i][j] + V[i+1][j]))/4)
+						- (((U[i-1][j] + U[i-1][j+1])*(V[i-1][j] + V[i][j]))/4))
+					+ (alpha/dx)*(((fabs(U[i][j] + U[i][j+1])*(V[i][j] - V[i+1][j]))/4)
+							- ((fabs(U[i-1][j] + U[i-1][j+1])*(V[i-1][j] - V[i][j]))/4));
+
+			dv2_y = (1/dy)*(pow((V[i][j] + V[i][j+1])/2, 2) - pow((V[i][j-1] + V[i][j])/2, 2))
+					+ (alpha/dy)*(((fabs(V[i][j] + V[i][j+1])*(V[i][j] - V[i][j+1]))/4)
+							- ((fabs(V[i][j-1] + V[i][j])*(V[i][j-1] - V[i][j]))/4));
+
+			G[i][j] = V[i][j] + dt*(((dv_x_2 + dv_y_2)/Re) - duv_x - dv2_y + GY);
+		}
+	}
+
+	/* Set boundary values */
+	for(i = 1; i < imax+1; i++) {
+		G[i][0] = V[i][0];
+		G[i][jmax] = V[i][jmax];
+	}
+
+	for(j = 1; j < jmax+1; j++) {
+		F[0][j] = U[0][j];
+		F[imax][j] = U[imax][j];
+	}
+}
+
+
+/**
+ * This operation computes the right hand side of the pressure poisson equation.
+ * The right hand side is computed according to the formula
+ *
+ * @f$ rs = \frac{1}{\delta t} \left( \frac{F^{(n)}_{i,j}-F^{(n)}_{i-1,j}}{\delta x} + \frac{G^{(n)}_{i,j}-G^{(n)}_{i,j-1}}{\delta y} \right)  @f$
+ *
+ */
+void calculate_rs(
+  double dt,
+  double dx,
+  double dy,
+  int imax,
+  int jmax,
+  double **F,
+  double **G,
+  double **RS
+)
+{
+	int i, j;
+
+	/* Iterate only over the inner cells */
+	for(i = 1; i <= imax; i++)
+	{
+		for(j = 1; j <= jmax; j++)
+		{
+			RS[i][j] = ( (F[i][j] - F[i-1][j])/dx + (G[i][j] - G[i][j-1])/dy)/dt;
+		}
+	}
+}
+
+
+/**
+ * Determines the maximal time step size. The time step size is restricted
+ * accordin to the CFL theorem. So the final time step size formula is given
+ * by
+ *
+ * @f$ {\delta t} := \tau \, \min\left( \frac{Re}{2}\left(\frac{1}{{\delta x}^2} + \frac{1}{{\delta y}^2}\right)^{-1},  \frac{{\delta x}}{|u_{max}|},\frac{{\delta y}}{|v_{max}|} \right) @f$
+ *
+ */
+void calculate_dt(
+  double Re,
+  double tau,
+  double *dt,
+  double dx,
+  double dy,
+  int imax,
+  int jmax,
+  double **U,
+  double **V
+)
+{
+	/* Only update dt if tau is positive */
+	if(tau > 0)
+	{
+		/* Find the max of both velocity matrices */
+		double u_max = matrix_abs_max(U, imax, jmax);
+		double v_max = matrix_abs_max(V, imax, jmax);
+
+		*dt = tau*fmin((Re/2)*(1/((1/pow(dx, 2)) + (1/pow(dy, 2)))), fmin(dx/u_max, dy/v_max));
+	}
+}
+
+
+/**
+ * Calculates the new velocity values according to the formula
+ *
+ * @f$ u_{i,j}^{(n+1)}  =  F_{i,j}^{(n)} - \frac{\delta t}{\delta x} (p_{i+1,j}^{(n+1)} - p_{i,j}^{(n+1)}) @f$
+ * @f$ v_{i,j}^{(n+1)}  =  G_{i,j}^{(n)} - \frac{\delta t}{\delta y} (p_{i,j+1}^{(n+1)} - p_{i,j}^{(n+1)}) @f$
+ *
+ * As always the index range is
+ *
+ * @f$ i=1,\ldots,imax-1, \quad j=1,\ldots,jmax @f$
+ * @f$ i=1,\ldots,imax, \quad j=1,\ldots,jmax-1 @f$
+ *
+ * @image html calculate_uv.jpg
+ */
+void calculate_uv(
+  double dt,
+  double dx,
+  double dy,
+  int imax,
+  int jmax,
+  double **U,
+  double **V,
+  double **F,
+  double **G,
+  double **P
+)
+{
+	int i, j;
+
+	/* Iterate only over the inner cells */
+	/* for U_ij */
+	for(i = 1; i <= imax-1; i++)
+	{
+		for(j = 1; j <= jmax; j++)
+		{
+			U[i][j] = F[i][j] - (dt/dx) * (P[i+1][j] - P[i][j]);
+		}
+	}
+
+	/* for V_ij */
+	for(i = 1; i <= imax; i++)
+	{
+		for(j = 1; j <= jmax-1; j++)
+		{
+			V[i][j] = G[i][j] - (dt/dy) * (P[i][j+1] - P[i][j]);
+		}
+	}
+}
