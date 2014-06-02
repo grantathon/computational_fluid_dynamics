67d81ced1536530941439bff658e16b90e439217
saleemayman <aiman1175@gmail.com>
2014-06-02 20:06:19 +0200

    fixed memalloc matrix dim in main.c

--- 04_assignment/main.c
+++ 04_assignment/main.c
@@ -171,18 +171,18 @@ int main(int argn, char** args)
             &rank_r, &rank_b, &rank_t, &omg_i, &omg_j, num_proc);
 
 	/* Initialize matrices for velocity, pressure, rhs, etc. */
-	init_uvp(UI, VI, PI, ir, jt, &U, &V, &P);
-	RS = matrix(0, ir+1, 0, jt+1);
-	F = matrix(0, ir+1, 0, jt+1);
-	G = matrix(0, ir+1, 0, jt+1);
+	init_uvp(UI, VI, PI, (ir - il), (jt - jb), &U, &V, &P);
+	F = matrix(0, (ir - il + 1), 0, (jt - jb + 1));
+	G = matrix(0, (ir - il + 1), 0, (jt - jb + 1));
+	RS = matrix(0, (ir - il + 1), 0, (jt - jb + 1));
 
 	/* Begin the time iteration process */
 	while(t < t_end)
 	{
-		calculate_dt(Re, tau, &dt, dx, dy, imax, jmax, U, V);
-		boundaryvalues(imax, jmax, U, V);
-		calculate_fg(Re, GX, GY, alpha, dt, dx, dy, imax, jmax, U, V, F, G);
-		calculate_rs(dt, dx, dy, imax, jmax, F, G, RS);
+		calculate_dt(Re, tau, &dt, dx, dy, imax, jmax, U, V);	/* FIX */
+		boundaryvalues(imax, jmax, U, V);	/* FIX */
+		calculate_fg(Re, GX, GY, alpha, dt, dx, dy, imax, jmax, U, V, F, G);	/* FIX */
+		calculate_rs(dt, dx, dy, imax, jmax, F, G, RS);	/* FIX */
 
 		it = 0;
 		do
@@ -193,12 +193,12 @@ int main(int argn, char** args)
 		while( it < itermax && res > eps);
 		printf("res=%f, it=%u ", res, it);
 
-		calculate_uv(dt, dx, dy, imax, jmax, U, V, F, G, P);
+		calculate_uv(dt, dx, dy, imax, jmax, U, V, F, G, P, il, ir, jb, jt, rank_l, rank_r, rank_b, rank_t);	/* FIX */
 
 		/* Visualize U, V, and P depending on dt_value */
 		if(((t / dt_value) >= visual_n) || (t == dt))
 		{
-			write_vtkFile(szProblem, visual_n, xlength, ylength, imax, jmax, dx, dy, U, V, P);
+			write_vtkFile(szProblem, visual_n, xlength, ylength, imax, jmax, dx, dy, U, V, P);	/* FIX */
 			visual_n++;
 		}
 
@@ -208,12 +208,12 @@ int main(int argn, char** args)
 	}
 
 	/* Deallocate heap memory */
-	free_matrix(U, 0, ir+1, 0, jt+1);
-	free_matrix(V, 0, ir+1, 0, jt+1);
-	free_matrix(P, 0, ir+1, 0, jt+1);
-	free_matrix(RS, 0, ir+1, 0, jt+1);
-	free_matrix(F, 0, ir+1, 0, jt+1);
-	free_matrix(G, 0, ir+1, 0, jt+1);
+	free_matrix(U, 0, (ir - il + 1), 0, (jt - jb + 1));
+	free_matrix(V, 0, (ir - il + 1), 0, (jt - jb + 1));
+	free_matrix(P, 0, (ir - il + 1), 0, (jt - jb + 1));
+	free_matrix(F, 0, (ir - il + 1), 0, (jt - jb + 1));
+	free_matrix(G, 0, (ir - il + 1), 0, (jt - jb + 1));
+	free_matrix(RS, 0, (ir - il + 1), 0, (jt - jb + 1));
 
 	/* Finalize MPI */
 	MPI_Barrier(MPI_COMM_WORLD);
